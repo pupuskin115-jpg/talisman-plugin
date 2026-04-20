c09c3aa4d2601ee48cd4cd0265be4d0b0ebe6c02
pupuskin115-jpg <pupuskin115@gmail.com>
2026-04-21 01:31:31 +0300

    Update TalismanManager.java

--- src/main/java/me/yourname/talisman/TalismanManager.java
+++ src/main/java/me/yourname/talisman/TalismanManager.java
@@ -3,27 +3,43 @@ package me.yourname.talisman;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.Material;
 
 public class TalismanManager {
 
-    public static ItemStack createTalisman(ItemStack item) {
-        ItemStack newItem = item.clone();
+    public static ItemStack createTalisman(String type) {
 
-        ItemMeta meta = newItem.getItemMeta();
+        ItemStack item;
 
-        meta.setDisplayName("§dТалисман");
+        switch (type) {
+            case "speed":
+                item = new ItemStack(Material.SUGAR);
+                break;
+            case "strength":
+                item = new ItemStack(Material.BLAZE_POWDER);
+                break;
+            case "health":
+                item = new ItemStack(Material.APPLE);
+                break;
+            default:
+                item = new ItemStack(Material.STONE);
+        }
+
+        ItemMeta meta = item.getItemMeta();
+
+        meta.setDisplayName("§dТалисман " + type);
         meta.setLore(java.util.List.of("§7Магический предмет"));
 
-        NamespacedKey key = new NamespacedKey(TalismanPlugin.getInstance(), "talisman");
+        NamespacedKey key = new NamespacedKey(TalismanPlugin.getInstance(), "talisman_type");
 
         meta.getPersistentDataContainer().set(
                 key,
-                org.bukkit.persistence.PersistentDataType.INTEGER,
-                1
+                org.bukkit.persistence.PersistentDataType.STRING,
+                type
         );
 
-        newItem.setItemMeta(meta);
+        item.setItemMeta(meta);
 
-        return newItem;
+        return item;
     }
 }
